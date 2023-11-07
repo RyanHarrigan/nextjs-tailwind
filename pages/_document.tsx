d43a4e90f6bad0a3b0088ca0b7650611e752c3af
Ryan Harrigan <ryan@changeprogramming.com>
2023-11-07 16:25:10 -0500

    update to dependencies

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,4 @@
 import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
-import {styled} from '@emotion/styled';
-
-const Body = styled.body``;
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -13,10 +10,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <Body className={'h-screen w-screen'}>
+        <body className={'h-screen w-screen'}>
           <Main />
           <NextScript />
-        </Body>
+        </body>
       </Html>
     )
   }
